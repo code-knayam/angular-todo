0b6d3b99337c489bf137e94d6535f4d40ed136eb
code-knayam <mayankjain1394@gmail.com>
2018-07-04 01:04:15 +0530

    First Commit for app architecture.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { UpperMenuComponent } from './upper-menu/upper-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    UpperMenuComponent
   ],
   imports: [
     BrowserModule

8a86796b4872094316385c98bdee53483bbd4675
code-knayam <mayankjain1394@gmail.com>
2018-07-22 21:15:31 +0530

    Added Completed Task flag.

--- src/app/core/task.model.ts
+++ src/app/core/task.model.ts
@@ -1,11 +1,15 @@
+import * as CONSTANTS from '../CONSTANTS';
+
 export class Task {
   public taskID: string;
   public taskName: string;
   public listID: string;
+  public completedStatus: boolean;
 
   constructor(taskID: string, taskName: string, listID: string) {
     this.taskID = taskID;
     this.taskName = taskName;
     this.listID = listID;
+    this.completedStatus = false;
   }
 }

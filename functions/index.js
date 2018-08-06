175b9d4f9102a3fe2d78f9c0d518a7eed1d49b5c
code-knayam <mayankjain1394@gmail.com>
2018-08-07 02:09:12 +0530

    Wrote first firebase cloud function.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
+admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.getUserInfo = functions.https.onRequest(
+	(req, res) => {
+		var db = admin.firestore();
+		db.collection('user-data').get().then(
+			(querySnapshot) => {
+				var data = [];
+				querySnapshot.forEach((doc) => {
+					data.push(doc.data());
+				});
+				res.send(data);
+				return "";
+			}
+		).catch(
+			(err) => {
+				res.send(err);
+			});
+	}
+)

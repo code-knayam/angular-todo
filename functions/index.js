e4014d1706b535de04712c8d3c26d659e0394c21
code-knayam <mayankjain1394@gmail.com>
2018-08-07 13:03:36 +0530

    Added backend API to get tasks.

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,7 @@ exports.getUserInfo = functions.https.onRequest(
 				var data = [];
 				querySnapshot.forEach((doc) => {
 					var userData = doc.data();
-					userData['id'] = doc.id;
+					userData['user_id'] = doc.id;
 					data.push(userData);
 				});
 				res.send(data);
@@ -23,17 +23,19 @@ exports.getUserInfo = functions.https.onRequest(
 				res.send(err);
 			});
 	}
-)
+);
 
 exports.getUserLists = functions.https.onRequest(
 	(req, res) => {
-		var userId = req.query.id;
+		var userId = req.query.userid;
 		var db = admin.firestore();
-		db.doc('user-data/'+ userId + '/lists_arr').get().then(
+		db.collection('user-data').doc(userId).collection('lists_arr').get().then(
 			(querySnapshot) => {
 				var data = [];
 				querySnapshot.forEach((doc) => {
-					data.push(doc.data());
+					var listData = doc.data();
+					listData['list_id'] = doc.id;
+					data.push(listData);
 				});
 				res.send(data);
 				return "";
@@ -43,4 +45,27 @@ exports.getUserLists = functions.https.onRequest(
 				res.send(err);
 			});
 	}
-)
+);
+
+exports.getUserTasks = functions.https.onRequest(
+	(req, res) => {
+		var userId = req.query.userid;
+		var listId = req.query.listid;
+		var db = admin.firestore();
+		db.collection('user-data').doc(userId).collection('lists_arr').doc(listId).collection('tasks_arr').get().then(
+			(querySnapshot) => {
+				var data = [];
+				querySnapshot.forEach((doc) => {
+					var taskData = doc.data();
+					taskData['task_id'] = doc.id;
+					data.push(taskData);
+				});
+				res.send(data);
+				return "";
+			}
+		).catch(
+			(err) => {
+				res.send(err);
+			});
+	}
+);
